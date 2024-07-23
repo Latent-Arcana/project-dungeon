e9c339d2b6d5e0439277e7eacd4d2a8f34d758e2
Henry Haner <clarkstraws@gmail.com>
2024-07-23 16:10:48 -0400

    adding a test

--- Assets/Scripts/BSP Classes/BSPGeneration.cs
+++ Assets/Scripts/BSP Classes/BSPGeneration.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1fc5c557bbf473ff27ac0dfef9fec27b208eb58789ef90661a68303ebdd20457
-size 31618
+oid sha256:e29a6e66c1c547d7d76e1c6bea72146c0d4f04f376f83095201d8b141f2999bd
+size 31705
