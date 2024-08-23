b9e0efca31ee8b9e29a8475f6696972f10e97e41
Kayleigh <latentartisan@gmail.com>
2024-08-22 21:38:19 -0400

    new sprites, update to dungeon narrator to highlight most recent item

--- Assets/Scripts/UI Scripts/DungeonNarrator.cs
+++ Assets/Scripts/UI Scripts/DungeonNarrator.cs
@@ -6,12 +6,13 @@ using UnityEngine.UIElements;
 
 public class DungeonNarrator : MonoBehaviour
 {
- 
+
     // Defining the narrator as a singleton
     public static DungeonNarrator Dungeon_Narrator { get; private set; }
 
 
     private UIDocument narrator_doc;
+    private TextElement text_narrator_top;
     private TextElement text_narrator;
 
     private void Awake()
@@ -29,29 +30,40 @@ public class DungeonNarrator : MonoBehaviour
 
         narrator_doc = this.GetComponent<UIDocument>();
         text_narrator = narrator_doc.rootVisualElement.Q("DungeonNarratorText") as TextElement;
+        text_narrator_top = narrator_doc.rootVisualElement.Q("DungeonNarratorTextTop") as TextElement;
+
         ClearDungeonNarratorText();
 
     }
 
     public void SetDungeonNarratorText(string message)
     {
-        text_narrator.text = message;
+        text_narrator_top.text = message;
+        text_narrator.text = "";
     }
 
     public void ClearDungeonNarratorText()
     {
-        SetDungeonNarratorText("");
+        text_narrator.text = "";
+        text_narrator_top.text = "";
     }
 
     public void AddDungeonNarratorText(string message)
     {
 
-        if(text_narrator.text.Length > 2500)
+        //move text down from the highlighted spot
+        text_narrator.text = text_narrator_top.text + "\n\n" + text_narrator.text;
+
+        //set highlighted text to message
+        text_narrator_top.text = message;
+
+
+        if (text_narrator.text.Length > 2500)
         {
             text_narrator.text = text_narrator.text.Remove(2000);
         }
         //TODO: This should eventually be truncated so it doesn't overflow the string variable
-        text_narrator.text = message + "\n\n" + text_narrator.text; //substring text_narrator.text to  anumber of lines or something
+        
     }
 
 
