f80271c06bc2aad8bcce78c1f71d45e2585bff8a
Henry Haner <clarkstraws@gmail.com>
2024-08-29 20:05:02 -0400

    player Inventory

---
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static ContainerGeneration;
+
+
+// TODO: Use inheritance so that our PlayerInventory and ContainerBehavior don't have so much code dupe between them
+public class PlayerInventory : MonoBehaviour
+{
+
+    [SerializeField]
+    public int maxItemCount; // this is where we're going to define how many items at a time can spawn in our object
+
+    private List<Item> items;
+    private ObjectBehavior objectData;
+
+    void Awake()
+    {
+        objectData = gameObject.GetComponent<ObjectBehavior>();
+    }
+
+    // We need to get container data for our container
+    void Start()
+    {
+        items = Container_Generator.GetItems(objectData.ObjectType, maxItemCount);
+
+    }
+
+    // TODO: INVENTORY INTERACTION
+    public List<Item> Open(){
+        return items;
+    }
+
+    void RemoveItem(Item item){
+        items.Remove(item);
+    }
+
+    void AddItem(Item item){
+        items.Add(item);
+    }
+
+
+    // DEBUG
+    public void InventoryDebugPrint()
+    {
+
+        Debug.Log("_________________________________________________");
+
+        Debug.Log($"The player is currently holding: ");
+
+        foreach (Item item in items)
+        {
+
+            Debug.Log(item.itemName);
+        }
+
+        Debug.Log("_________________________________________________");
+    }
+
+
+}
