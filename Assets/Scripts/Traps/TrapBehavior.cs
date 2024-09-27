b546c5fb90b1e62ebf43f5a57aebcc1838655412
Henry Haner <clarkstraws@gmail.com>
2024-09-27 15:41:06 -0400

    traps updated and working!

--- Assets/Scripts/Traps/TrapBehavior.cs
+++ Assets/Scripts/Traps/TrapBehavior.cs
@@ -16,6 +16,7 @@ public class TrapBehavior : MonoBehaviour
     private PlayerMovement playerMovement;
     private bool playerInRoom;
 
+    public PlayerStatsManager Player_Stats;
 
     public void Awake()
     {
@@ -43,12 +44,10 @@ public class TrapBehavior : MonoBehaviour
         float spawnChance = UnityEngine.Random.value;
         int prefabChoice = UnityEngine.Random.Range(0, projectilePrefabs.Length);
 
-        if (spawnChance <= .25f && playerInRoom)
+        if (spawnChance <= 0.85f && playerInRoom)
         {
-            //Debug.Log("Spawn a trap projectile");
-
+            //Debug.Log("spawn chance is " + spawnChance + " and it needed to be lower than 0.85f");
             SpawnProjectile(projectilePrefabs[prefabChoice]);
-
         }
 
         Physics2D.SyncTransforms();
@@ -91,6 +90,8 @@ public class TrapBehavior : MonoBehaviour
                 5 6 7
         */
 
+        //Debug.Log("Spawn choice is: " + randomSpawnChoice);
+
         switch (randomSpawnChoice)
         {
             case 0:
@@ -100,43 +101,38 @@ public class TrapBehavior : MonoBehaviour
 
             case 1:
                 direction = Vector3.up;
-
                 spawnPosition = originPosition + direction;
                 break;
 
             case 2:
                 direction = Vector3.right + Vector3.up;
-
                 spawnPosition = originPosition + direction;
                 break;
 
             case 3:
                 direction = Vector3.left;
-
                 spawnPosition = originPosition + direction;
                 break;
+
             case 4:
                 direction = Vector3.right;
-
                 spawnPosition = originPosition + direction;
                 break;
+                
             case 5:
                 direction = Vector3.down + Vector3.left;
-
                 spawnPosition = originPosition + direction;
                 break;
+
             case 6:
                 direction = Vector3.down;
-
                 spawnPosition = originPosition + direction;
-
                 break;
+
             case 7:
                 direction = Vector3.right + Vector3.down;
-
                 spawnPosition = originPosition + direction;
                 break;
-
 
             default:
                 direction = Vector3.zero;
@@ -149,7 +145,7 @@ public class TrapBehavior : MonoBehaviour
         LayerMask mask = ~(1 << LayerMask.NameToLayer("ObjectPlacementLayer")); // we want to ignore the placement layer that we used for creating objects  in each scene
         Collider2D collision = Physics2D.OverlapCircle(checkPosition, 0.1f, mask);
 
-        if (collision == null)
+        if (collision == null || collision.tag == "enemy")
         {
             GameObject proj = Instantiate(projectile, spawnPosition, Quaternion.identity);
             ProjectileBehavior projectileBehavior = proj.GetComponent<ProjectileBehavior>();
@@ -157,7 +153,11 @@ public class TrapBehavior : MonoBehaviour
             projectileBehavior.trap = gameObject;
             projectileBehavior.projectileId = 0;
             projectileBehavior.isAtSpawn = true;
+            projectileBehavior.Player_Stats = Player_Stats;
         }
+        // else{
+        //     Debug.Log("did not spawn at " + spawnPosition + " because we ran into " + collision.gameObject.name);
+        // }
 
         return;
     }
