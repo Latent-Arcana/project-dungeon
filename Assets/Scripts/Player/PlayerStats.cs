169cbc413d43ad9b90468f8e4b21a96f25143927
Kayleigh <latentartisan@gmail.com>
2024-12-27 16:15:56 -0500

    WHY WAS FLEX GROW SET TO 1 AAAAAAAAAAA

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
-using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
